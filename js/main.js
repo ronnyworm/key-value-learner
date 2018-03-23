df8932b339b5337fb21eb3042bc94b885fc3b073
Ronny Worm <r-worm@gmx.de>
2018-03-23 14:04:21 +0100

    The rows are now shuffled - always differently

--- js/main.js
+++ js/main.js
@@ -14,14 +14,27 @@ jQuery(document).ready(function($){
 		$("#" + id).css('color', 'black');
 	}
 
+	function shuffle(a) {
+	    var j, x, i;
+	    for (i = a.length - 1; i > 0; i--) {
+	        j = Math.floor(Math.random() * (i + 1));
+	        x = a[i];
+	        a[i] = a[j];
+	        a[j] = x;
+	    }
+	}
+
 	function generateRows(headers, pairs) {
 		result = "";
+
+		indices = Array.apply(null, Array(pairs.length)).map(function (_, i) {return i;});
+		shuffle(indices);
 
 		nummer = 0;
 		for(var i = 0, l = pairs.length; i < l; i++){
 			result += "<div class='row'>";
-			result += "<div class='cell' data-title='" + headers[0] + "'>" + pairs[i][0] + "</div>";
-			result += "<div onclick='window.show(" + nummer + ")' id='" + nummer + "' style='color:white' class='cell' data-title='" + headers[1] + "'>" + pairs[i][1] + "</div>";
+			result += "<div class='cell' data-title='" + headers[0] + "'>" + pairs[indices[i]][0] + "</div>";
+			result += "<div onclick='window.show(" + nummer + ")' id='" + nummer + "' style='color:white' class='cell' data-title='" + headers[1] + "'>" + pairs[indices[i]][1] + "</div>";
 			result += "</div>";
 
 			nummer += 1;
